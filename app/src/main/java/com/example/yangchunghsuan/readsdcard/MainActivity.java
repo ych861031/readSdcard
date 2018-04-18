8179f7205eb2561abacdc0676e6c2ebef1205b0e
ych861031 <danny861031@gmail.com>
2018-04-18 22:22:47 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yangchunghsuan/readsdcard/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.yangchunghsuan.readsdcard;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.io.File;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        int permission = ActivityCompat.checkSelfPermission(this,READ_EXTERNAL_STORAGE);
+        if (permission!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[] {READ_EXTERNAL_STORAGE},1);
+        }else {
+            File[] file = Environment.getExternalStorageDirectory().listFiles();
+        }
+
+
+
+    }
+}
